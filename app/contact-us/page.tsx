1fa3866b948014bfaacf167325a0497324ee00e7
Sourav Bhowal <souravbhowal15@gmail.com>
2026-09-23 12:41:44 +0530

    Refactor home page structure by moving AboutSection, ContactSection, and HeroSection to shared components. Update their implementations to utilize data from home-page.data for dynamic content. Adjust Navbar links for improved routing consistency.

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -5,7 +5,8 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Contact Us",
-  description: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has electronic typesetting, remaining essentially unchanged Lorem Ipsum has been the industry's standard dummy.",
+  description:
+    "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has electronic typesetting, remaining essentially unchanged Lorem Ipsum has been the industry's standard dummy.",
 };
 
 export default function ContactUsPage() {
--- lib/blogs-page.data.ts
+++ lib/blogs-page.data.ts
@@ -1,24 +1,39 @@
-const blogs = [
+interface Blog {
+  title: string;
+  description: string;
+  image: string;
+  author: string;
+  date: string;
+  slug: string;
+}
+
+const blogs: Blog[] = [
   {
     title: "Blog 1",
     description:
       "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has electronic typesetting, remaining essentially unchanged Lorem Ipsum has been the industry's standard dummy.",
-    imageSrcDesktop: "/images/blog-1.png",
-    imageSrcMobile: "/images/blog-1-m.png",
+    image: "/images/product-1.png",
+    author: "John Doe",
+    date: "2026-01-01",
+    slug: "blog-1",
   },
   {
     title: "Blog 2",
     description:
       "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has electronic typesetting, remaining essentially unchanged Lorem Ipsum has been the industry's standard dummy.",
-    imageSrcDesktop: "/images/blog-2.png",
-    imageSrcMobile: "/images/blog-2-m.png",
+    image: "/images/product-2.png",
+    author: "Jane Doe",
+    date: "2026-01-02",
+    slug: "blog-2",
   },
   {
     title: "Blog 3",
     description:
       "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has electronic typesetting, remaining essentially unchanged Lorem Ipsum has been the industry's standard dummy.",
-    imageSrcDesktop: "/images/blog-3.png",
-    imageSrcMobile: "/images/blog-3-m.png",
+    image: "/images/product-1.png",
+    author: "John Doe",
+    date: "2026-01-03",
+    slug: "blog-3",
   },
 ];
 
