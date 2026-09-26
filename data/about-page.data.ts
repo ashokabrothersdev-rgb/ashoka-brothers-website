efd0d38d019e3ded39b1c4d56d1e3296a95e4a03
Sourav Bhowal <souravbhowal15@gmail.com>
2026-09-26 13:27:58 +0530

    Add ProductGridSection to ProductsPage and enhance product data structure

--- data/about-page.data.ts
+++ data/about-page.data.ts
@@ -1,8 +1,49 @@
+const visionImages = [
+  {
+    src: "/images/about-1.png",
+    alt: "Vision 1",
+  },
+  {
+    src: "/images/about-1.png",
+    alt: "Vision 2",
+  },
+  {
+    src: "/images/about-1.png",
+    alt: "Vision 3",
+  },
+];
+
+const carouselData = [
+  {
+    src: "/images/about-2.png",
+    alt: "Carousel 1",
+    title: "Quality Assurance",
+    badge: "Quality",
+    description: "We ensure that all our products are of the highest quality.",
+  },
+  {
+    src: "/images/about-2.png",
+    alt: "Carousel 2",
+    title: "Customer Satisfaction",
+    badge: "Customer",
+    description: "We ensure that all our products are of the highest quality.",
+  },
+  {
+    src: "/images/about-2.png",
+    alt: "Carousel 3",
+    title: "Customer Satisfaction",
+    badge: "Customer",
+    description: "We ensure that all our products are of the highest quality.",
+  },
+];
+
 export const aboutPage = {
-    title: "About Ashoka Brothers",
-    description:
-      "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has electronic typesetting, remaining essentially unchanged Lorem Ipsum has been the industry's standard dummy.",
-    isVideo: false,
-    imageSrcDesktop: "/images/screw-hero.png",
-    imageSrcMobile: "/images/screw-hero-m.png",
-  };
+  title: "About Ashoka Brothers",
+  description:
+    "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has electronic typesetting, remaining essentially unchanged Lorem Ipsum has been the industry's standard dummy.",
+  isVideo: false,
+  imageSrcDesktop: "/images/screw-hero.png",
+  imageSrcMobile: "/images/screw-hero-m.png",
+  visionImages,
+  carouselData,
+};
